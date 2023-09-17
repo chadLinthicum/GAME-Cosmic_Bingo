654613df42e9801fb63714b643b9cf50eeb340e3
chad <portableanswers@gmail.com>
2023-09-17 19:49:06 -0400

    Bingo machine physics working.

---
+++ Assets/Scripts/Air.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Air : MonoBehaviour
+{
+    public float bounceForce = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Air"))
+        {
+    Rigidbody rb = GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
+            }
+        }
+    }
+   
+}
