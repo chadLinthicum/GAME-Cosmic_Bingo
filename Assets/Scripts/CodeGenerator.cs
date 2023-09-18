0b845ecb8c51fe7978036ffdad08b41f46135f7c
chad <portableanswers@gmail.com>
2023-09-17 22:54:04 -0400

    Bingo card generating.

--- Assets/Scripts/CodeGenerator.cs
+++ Assets/Scripts/CodeGenerator.cs
@@ -1,22 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CodeGenerator : MonoBehaviour
 {
-    
     string[] SPACE = { "S", "P", "A", "C", "E" };
-    
+    public TextMeshProUGUI displayText;
+
     void Start()
     {
-        
-    }
+        int randomLetter;
+        int randomInt;
+        string square;
+        HashSet<string> generatedSquares = new HashSet<string>();
 
-    
-    void Update()
-    {
-        int randomLetter = (int)Random.Range(0f, 5f);
-        int randomInt = (int)Random.Range(1f, 76f);
-        Debug.Log(SPACE[randomLetter]+randomInt);
+        for (int i = 0; i < 25; i++)
+        {
+            do
+            {
+                randomLetter = (int)Random.Range(0f, 5f);
+                randomInt = (int)Random.Range(1f, 76f);
+                if(i != 12)
+                {
+                    square = SPACE[randomLetter] + randomInt;
+                }
+                else
+                {
+                    square = "FRE";
+                }
+                
+            }
+            while (generatedSquares.Contains(square));
+
+            generatedSquares.Add(square);
+
+            if (square.Length == 2)
+            {
+                square += " "; // Add a space to the end of the square if its length is 2
+            }
+
+            displayText.text += square;
+
+            if ((i + 1) % 5 == 0 && i != 0)
+            {
+                displayText.text += "\n"; // Append a line break after every 5th instance
+            }
+            else
+            {
+                displayText.text += " "; // Append a space between each instance
+            }
+        }
     }
 }
