fbe74c444784a29d51df16be60060c2b78a0eac3
chad <portableanswers@gmail.com>
2023-10-08 10:45:40 -0400

    Balls now spawn as a child to the parent game object.

--- Assets/Scripts/BallSpawn.cs
+++ Assets/Scripts/BallSpawn.cs
@@ -13,10 +13,11 @@ public class BallSpawn : MonoBehaviour
     {
         for (int i=0; i<19; i++)
         {
-            Instantiate(ball1, transform.position, Quaternion.identity);
-            Instantiate(ball2, transform.position, Quaternion.identity);
-            Instantiate(ball3, transform.position, Quaternion.identity);
-            Instantiate(ball4, transform.position, Quaternion.identity);
+            //This will spawn the prefabs as a child to the game object it is attached to
+            Instantiate(ball1, transform);
+            Instantiate(ball2, transform);
+            Instantiate(ball3, transform);
+            Instantiate(ball4, transform);
         }
     }
 
